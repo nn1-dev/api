038ed2911d9cf15cbf95c98de26e30bd52aea664
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2025-12-01 07:22:37 +0000

    revamp an api structure

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,62 @@
-import { Hono, Context, Next } from "hono";
+import { Hono } from "hono";
+import { logger } from "hono/logger";
 
-export interface Env {
-  DB: D1Database;
-  AUTH: string;
-}
+const app = new Hono<{ Bindings: Cloudflare.Env }>();
 
-export default {
-  async fetch(request, env, ctx) {
-    const app = new Hono<{ Bindings: Env }>();
+// middlewares
+app.use(logger());
+app.use(async (c, next) => {
+  console.log({ envAuth: c.env.AUTH });
+  const authHeader = c.req.header("Authorization");
+  if (!authHeader) {
+    return c.json({ status: "error", data: "Unauthorized" }, 401);
+  }
 
-    const authMiddleware = async (c: Context, next: Next) => {
-      const authHeader = c.req.header("Authorization");
-      if (!authHeader) {
-        return c.json({ error: "Unauthorized" }, 401);
-      }
+  const token = authHeader.startsWith("Bearer ")
+    ? authHeader.substring(7)
+    : authHeader;
 
-      const token = authHeader.startsWith("Bearer ")
-        ? authHeader.substring(7)
-        : authHeader;
+  if (token !== c.env.AUTH) {
+    return c.json({ status: "error", data: "Unauthorized" }, 401);
+  }
 
-      if (token !== env.AUTH) {
-        return c.json({ error: "Unauthorized" }, 401);
-      }
+  return next();
+});
 
-      return next();
-    };
+// 404
+app.notFound((c) =>
+  c.json(
+    {
+      status: "error",
+      data: "The route was not found",
+    },
+    404,
+  ),
+);
 
-    app.get("/", authMiddleware, async (c) => {
-      try {
-        const stmt = env.DB.prepare("SELECT * FROM comments LIMIT 3");
-        const { results } = await stmt.all();
+// 500
+app.onError((err, c) =>
+  c.json(
+    {
+      status: "error",
+      data: `Internal Server Error: ${err.message}`,
+    },
+    500,
+  ),
+);
 
-        return c.json(results);
-      } catch (error: any) {
-        return c.json({ error: error.message }, 500);
-      }
-    });
+// subscribers
+app.get("/subscribers", (c) => c.text("Subscribers list"));
+app.get("/subscribers/:id", (c) => c.text("Subscribers get"));
+app.post("/subscribers", (c) => c.text("Subscribers create"));
+app.delete("/subscribers/:id", (c) => c.text("Subscribers delete"));
 
-    return app.fetch(request, env, ctx);
-  },
-} satisfies ExportedHandler<Env>;
+// tickets
+app.get("/tickets", (c) => c.text("Tickets list"));
+app.get("/tickets/:eventId", (c) => c.text("Tickets event list"));
+app.get("/tickets/:eventId/:ticketId", (c) => c.text("Tickets get"));
+app.post("/tickets", (c) => c.text("Tickets create"));
+app.put("/tickets/:eventId/:ticketId", (c) => c.text("Tickets update"));
+app.delete("/tickets/:eventId/:id", (c) => c.text("Tickets delete"));
+
+export default app;
