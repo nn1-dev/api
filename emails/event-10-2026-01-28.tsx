c7ce07d623dabc485e9a4ce9a6f94f285c30f9f5
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2026-01-20 08:03:43 +0000

    emails prep before the event

---
+++ emails/event-10-2026-01-28.tsx
@@ -0,0 +1,90 @@
+import * as React from "react";
+import { render } from "@react-email/render";
+import Layout from "../components/Layout.tsx";
+import Text from "../components/Text.tsx";
+import Button from "../components/Button.tsx";
+import Link from "../components/Link.tsx";
+import Heading from "../components/Heading.tsx";
+import Hr from "../components/Hr.tsx";
+import Socials from "../components/Socials.tsx";
+import Schedule from "../components/Schedule.tsx";
+
+interface EmailProps {
+  ticketUrl: string;
+}
+
+export const Email = ({ ticketUrl }: EmailProps) => (
+  <Layout>
+    <Text>
+      <strong>NN1 Dev Club #10</strong> is tomorrow and we are looking forward
+      to seeing you. Just a quick reminder with all the details and the link to
+      your ticket.
+    </Text>
+    <Text>
+      🗓️ Thursday, 29/01/2026, 18:00
+      <br />
+      📍{" "}
+      <Link href="https://maps.app.goo.gl/q7RFeDME5cLZWPFA7">
+        Vulcan Works, Northampton, NN1 1EW
+      </Link>
+      <br />
+      🅿️{" "}
+      <Link href="https://maps.app.goo.gl/fk3jpaPLSLGrHNmH6">
+        St. John's Multi Storey Car Park
+      </Link>
+    </Text>
+
+    <Button href={ticketUrl}>Open Your Ticket</Button>
+    <Hr />
+    <Heading>Schedule</Heading>
+    <Schedule
+      items={[
+        {
+          timeStart: "18:00",
+          timeEnd: "18:30",
+          title: "Meet and Greet",
+        },
+        {
+          timeStart: "18:30",
+          timeEnd: "19:00",
+          title: '"Bad at CSS" by Keith Cirkel',
+          descriptoin:
+            'CSS is a beautiful nightmare of cascading chaos. To be truly bad at it requires mastery of arcane syntax. Together we’ll dive into the depths of the cascade to explore the "wrong" ways to write CSS - the hacks, the horrors, and the head-scratchers - and uncover the logic (or lack thereof) behind the language’s most unhinged features. You’ll leave this talk worse at CSS… and somehow better for it.',
+        },
+        {
+          timeStart: "19:00",
+          timeEnd: "19:15",
+          title: "Break",
+        },
+        {
+          timeStart: "19:15",
+          timeEnd: "19:45",
+          title:
+            '"Start-Up to Exit: The Journey for Tech Companies" by Samantha Wildman and Ian Zant-Boer',
+          descriptoin:
+            "From idea to investment to exit, every stage of a tech company’s journey brings new financial and strategic challenges. In this session, Samantha Wildman and Ian Zant-Boer share insights from their work advising high-growth technology businesses on how to build a finance foundation that attracts investors, supports innovation, and maximises long-term value. Samantha will explore the key financial levers that drive growth, how to prepare for fundraising rounds and exits. Ian Zant-Boer will offer a legal and investor lens on scaling, sharing his experience as a venture capital founder and adviser to scale-ups on how to structure for investment and avoid common legal pitfalls during growth and exit.",
+        },
+      ]}
+    />
+
+    <Hr />
+    <Text>
+      NN1 Dev Club Crew,
+      <br />
+      Pawel & Darren
+    </Text>
+    <Socials />
+  </Layout>
+);
+
+Email.PreviewProps = {
+  ticketUrl: "https://nn1.dev/events/5/123",
+} as EmailProps;
+
+const renderEmailEvent_9_2025_11_26 = async ({ ticketUrl }: EmailProps) => ({
+  html: await render(<Email ticketUrl={ticketUrl} />),
+  text: await render(<Email ticketUrl={ticketUrl} />, { plainText: true }),
+});
+
+export default Email;
+export { renderEmailEvent_9_2025_11_26 };
---
+++ emails/newsletter-2026-01-27.tsx
@@ -0,0 +1,72 @@
+import { render } from "@react-email/render";
+import Button from "../components/Button";
+import Hr from "../components/Hr";
+import Link from "../components/Link";
+import Text from "../components/Text";
+import Layout from "../components/Layout";
+import Socials from "../components/Socials";
+
+interface EmailProps {
+  unsubscribeUrl: string;
+}
+
+export const Email = ({ unsubscribeUrl }: EmailProps) => (
+  <Layout>
+    <Text>
+      A quick reminder that our next meetup is happening in two days and we
+      still have a few slots left! We have three amazing speakers lined up for
+      you this time: <strong>Keith Cirkel</strong>,{" "}
+      <strong>Samantha Wildman</strong> and <strong>Ian Zant-Boer</strong>. We
+      would love to see you there! As always the event is free but registration
+      is required.
+    </Text>
+    <Text>
+      🗓️ Thursday, 29/01/2026, 18:00
+      <br />
+      📍 Vulcan Works, Northampton, NN1 1EW
+      <br />
+      💰 Free (registration required, limited to 100)
+    </Text>
+    <Button href="https://nn1.dev/events/10">Register now</Button>
+    <Hr />
+    <Text>
+      Code written by <strong>Keith Cirkel</strong> is present in all dominant
+      browser engines, and as a Staff Engineer at Mozilla leading the DOM Core
+      team, you can tell he knows something about browsers. In his talk, he's
+      going to dive into the depths of CSS and the language's most unhinged
+      features.
+    </Text>
+    <Text>
+      <strong>Samantha Wildman</strong> and <strong>Ian Zant-Boer</strong> are
+      going to share insights from their work advising high-growth technology
+      businesses on how to build a financial foundation that attracts investors,
+      supports innovation, and maximises long-term value. From idea to
+      investment to exit.
+    </Text>
+    <Hr />
+    <Text>Questions? Reach out on social media or reply to this email!</Text>
+    <Text>
+      NN1 Dev Club Crew,
+      <br />
+      Pawel & Darren
+    </Text>
+    <Socials />
+    <Hr />
+    <Text>
+      Are you no longer interested? You can{" "}
+      <Link href={unsubscribeUrl}>unsubscribe</Link> anytime.
+    </Text>
+  </Layout>
+);
+
+Email.PreviewProps = {
+  unsubscribeUrl: "#",
+} as EmailProps;
+
+const renderEmailNewsletter_2026_01_14 = async (props: EmailProps) => ({
+  html: await render(<Email {...props} />),
+  text: await render(<Email {...props} />, { plainText: true }),
+});
+
+export default Email;
+export { renderEmailNewsletter_2026_01_14 };
