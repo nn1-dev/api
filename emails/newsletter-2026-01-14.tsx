b7699394d06f4506e5bab0dde68f54de055f9c30
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2026-02-08 09:46:14 +0000

    up: missed one

--- emails/newsletter-2026-01-14.tsx
+++ emails/newsletter-2026-01-14.tsx
@@ -56,7 +56,7 @@ export const Email = ({ unsubscribeUrl }: EmailProps) => (
 );
 
 Email.PreviewProps = {
-  unsubscribeUrl: "#",
+  unsubscribeUrl: "https://nn1.dev",
 } as EmailProps;
 
 const renderEmailNewsletter_2026_01_14 = async (props: EmailProps) => ({
